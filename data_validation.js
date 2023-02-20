4a8c6cf0f24e604dadd5f4d07fe9ea78ae1b2d26
Atharva Kiran Chaudhari <92793651+Atharva-C@users.noreply.github.com>
2023-02-20 22:21:46 +0530

    Add files via upload

---
+++ data_validation.js
@@ -0,0 +1,35 @@
+const form = document.getElementById("registration-form");
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const address = document.getElementById("address").value;
+  const phone = document.getElementById("phone").value;
+
+  if (name === "" || email === "" || password === "" || address === "" || phone === "") {
+    alert("All fields are required");
+    return false;
+  }
+
+  if (phone.length !== 10) {
+    alert("Phone number must be 10 digits");
+    return false;
+  }
+
+  if (/\d/.test(name)) {
+    alert("Name cannot contain digits");
+    return false;
+  }
+
+  if (!/\d/.test(password) || password.length < 8) {
+    alert("Password must contain at least one number and be at least 8 characters long");
+    return false;
+  }
+
+  alert(`Name: ${name}
+  Email: ${email}
+  Password: ${password}
+  Address: ${address}
+  Phone: ${phone}`);
+});
